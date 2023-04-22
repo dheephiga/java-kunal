0fa7996918db92f2f55c7dece374d68e99f3dad2
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-04-22 20:59:06 +0530

    initial commit

--- src/lec01/Main.java
+++ src/lec01/Main.java
@@ -2,6 +2,6 @@ package lec01;
 
 public class Main {
     public static void main(String args[]){
-        System.out.println("Hello wordl");
+        System.out.println("Hello world");
     }
 }
---
+++ src/lec01/output.java
@@ -0,0 +1,13 @@
+package lec01;
+
+import java.util.Scanner;
+
+public class output{
+	public static void main(String args[]){
+		System.out.println("Hey how are you?");
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println(input.nextLine());
+
+	}
+}
