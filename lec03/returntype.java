6b4f4ecaf637fa1d1b88e586f00dfe3105a3bbff
Dheephiga <64630313+dheephiga@users.noreply.github.com>
2023-04-24 21:53:23 +0530

    adding programs

--- lec03/returntype.java
+++ lec03/returntype.java
@@ -3,15 +3,12 @@ package lec03;
 public class returntype{
 	public static void main(String args[]){
 		String msg = greeting();
-		System.out.println(msg);
+		System.out.println(msg + "This is printed using return type function - string");
 	}
 
 	static String greeting()
 	{
 		String greet = "Hello Java";
 		return greet;
-
 	}
 }
-
-
